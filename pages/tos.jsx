a2b9d15c17dbb10d26eb551bc52e5dbdf8f50110
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 03:10:11 +0530

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,19 +2,37 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 25/03/24 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ 1. Acceptance of Terms
+By adding Gojo, the Discord music bot, to your server or by using any of its services, you agree to be bound by these Terms of Service. If you do not agree with any part of these terms, you may not use Gojo.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ 2. Use of Services
+Gojo is a Discord music bot designed to provide music streaming services within Discord servers. You may use Gojo in accordance with its intended purpose and within the guidelines outlined in these Terms of Service.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ 3. User Conduct
+You agree to use Gojo in a manner that complies with all applicable laws and regulations. You may not use Gojo to engage in any unlawful or prohibited activities, including but not limited to:
+- Violating Discord's Terms of Service or Community Guidelines
+- Distributing or sharing illegal or copyrighted content
+- Engaging in harassment, hate speech, or other forms of abusive behavior
+- Exploiting or disrupting Gojo's services or its users</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ 4. User Content
+By using Gojo, you understand and agree that any content you upload, transmit, or otherwise make available through the bot is your responsibility. You retain ownership of any intellectual property rights in the content you provide.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ 5. Privacy
+Your privacy is important to us. Please refer to our Privacy Policy for information on how we collect, use, and disclose your personal information in connection with Gojo.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
+<p>⊳ 6. Termination
+We reserve the right to terminate or suspend your access to Gojo at any time, with or without cause, and without prior notice. Upon termination, all rights granted to you under these Terms of Service will cease immediately.</p>
+<div className="py-3"></div>
+<p>⊳ 7. Limitation of Liability
+In no event shall we be liable for any indirect, incidental, special, consequential, or punitive damages, including but not limited to loss of profits, data, or use, arising out of or in connection with your use of Gojo.</p>
+<div className="py-3"></div>
+<p>⊳ 8. Changes to Terms
+We reserve the right to update or modify these Terms of Service at any time. We will notify users of any material changes by posting the updated terms on our website or through other communication channels.</p>
+<div className="py-3"></div>
+<p>⊳ Contact Us: If you have any questions or concerns about this Terms of Service or our use of your personal information, please contact us at the discord support server provided within the bot's help menu or in footer.
 </div>
     </div>
   );
