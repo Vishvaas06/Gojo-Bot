1f9f2753e3f5a53e7a78d06a9d88998a05188c00
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 01:10:13 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function Index() {
           Gojo
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Gojo? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+      ✨Gojo is your ultimate companion for bringing music to life within your Discord server. Totally FREE with High Quality. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
@@ -89,9 +89,9 @@ Vote Gojo</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Gojo, the best and most up-to-date bot?</p>
+                  <img src="https://i.ibb.co/bJx2mYR/6ed61a190867629-65c20bf0d3b31.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold">Yowai Mo!</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Maybe I should just kill all the higher-ups, Don't you agree?</p>
                 </div>
                 <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
