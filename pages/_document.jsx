a96d5f711fafc2726d23fa6f4a9e8be287852ab6
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 00:30:01 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,37 +6,37 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" href="/logo.jpg" />
+          <link rel="apple-touch-icon" href="/logo.png" />
           <meta name="robots" content="index,follow" />
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="✨Gojo is your ultimate companion for bringing music to life within your Discord server. Totally FREE with High Quality."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@Gojo" />
+          <meta name="twitter:creator" content="@Gojo" />
+          <meta property="og:url" content="https://gojo-bot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Gojo" />
           <link
             rel="icon"
-            href="/img/logo2.png"
+            href="/img/logo.png"
             type="image/x-icon"
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="✨Gojo is your ultimate companion for bringing music to life within your Discord server. Totally FREE with High Quality."
           />
-          <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image" content="/img/bck.jpeg" />
+          <meta property="og:image:alt" content="Gojo" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Gojo"
           />
-          <meta name="theme-color" content="#087ddd" />
-          <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
+          <meta name="theme-color" content="#0a9afa" />
+          <link rel="icon" href="/img/logo.png" type="image/x-icon" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
