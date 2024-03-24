fd615a41b60ae4c595eff93b6627a0abf8d14748
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 03:04:19 +0530

    Update gems.jsx

--- pages/gems.jsx
+++ pages/gems.jsx
@@ -14,7 +14,7 @@ export default function Partners() {
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-
+                
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/N7y3SE3prf">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
@@ -31,22 +31,6 @@ export default function Partners() {
                     </a>
                     </div>
 
-
-                            <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
-                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1172657853056684102/1172671829673005146/maskot_profil_alev.png" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Nodesty VDS</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                   DISCOUNT CODE: CODESHARE Experience uninterrupted internet speed up to 10Gb and cheap intel i9, amd ryzen series vds with Nodesty. Shop with Code Share assurance.
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                    </a>
-                    </div>
 
                 </div>
                 <div className="py-10"></div>
