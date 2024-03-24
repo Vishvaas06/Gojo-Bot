aae5a62c298ddbf122a8b1431158e6b6e52c01fc
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 02:32:38 +0530

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -9,21 +9,21 @@ export default function Partners() {
       </div>
                 <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
                     <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
+                    They Trust Us
                 </h1>
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me">
+            <a href="https://discord.gg/N7y3SE3prf">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/72826a4ed7d1a010df263623be5dd4ba.png" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1221402358005633054/1221561013984428043/IMG_5232.png?ex=66130659&is=66009159&hm=d26d2c8b4eca01bca2b82ea56af1e4375ef0d5645867b99b069e9fbfcfe4feec&=&format=webp&quality=lossless" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Vivi's Donation Community</h1>        
                                 <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                      Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.
+                                    <p className="font-normal fa-users font-sm">
+                                      600+ members
                                     </p>
                                 </div>     
                             </div>
