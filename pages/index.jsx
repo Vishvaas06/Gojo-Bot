7b052913c57201726787a662a2309c761354cc05
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-24 22:27:40 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function Index() {
           Gojo
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Gojo? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+       ✨Gojo is your ultimate companion for bringing music to life within your Discord server. Totally FREE with High Quality. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
