ab2e1858b62bda2ab296d217ebeee98e664f6fd1
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 03:16:16 +0530

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,13 +4,13 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 25/03/24 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ 1. Acceptance of Terms
+<p>⊳ 1. Acceptance of Terms<br></br>
 By adding Gojo, the Discord music bot, to your server or by using any of its services, you agree to be bound by these Terms of Service. If you do not agree with any part of these terms, you may not use Gojo.</p>
 <div className="py-3"></div>
-<p>⊳ 2. Use of Services
+<p>⊳ 2. Use of Services<br></br>
 Gojo is a Discord music bot designed to provide music streaming services within Discord servers. You may use Gojo in accordance with its intended purpose and within the guidelines outlined in these Terms of Service.</p>
 <div className="py-3"></div>
-<p>⊳ 3. User Conduct
+<p>⊳ 3. User Conduct<br></br>
 You agree to use Gojo in a manner that complies with all applicable laws and regulations. You may not use Gojo to engage in any unlawful or prohibited activities, including but not limited to:
 <br></br>
 - Violating Discord's Terms of Service or Community Guidelines
@@ -21,19 +21,19 @@ You agree to use Gojo in a manner that complies with all applicable laws and reg
 <br></br>
 - Exploiting or disrupting Gojo's services or its users</p>
 <div className="py-3"></div>
-<p>⊳ 4. User Content
+<p>⊳ 4. User Content<br></br>
 By using Gojo, you understand and agree that any content you upload, transmit, or otherwise make available through the bot is your responsibility. You retain ownership of any intellectual property rights in the content you provide.</p>
 <div className="py-3"></div>
-<p>⊳ 5. Privacy
+<p>⊳ 5. Privacy<br></br>
 Your privacy is important to us. Please refer to our Privacy Policy for information on how we collect, use, and disclose your personal information in connection with Gojo.</p>
 <div className="py-3"></div>
-<p>⊳ 6. Termination
+<p>⊳ 6. Termination<br></br>
 We reserve the right to terminate or suspend your access to Gojo at any time, with or without cause, and without prior notice. Upon termination, all rights granted to you under these Terms of Service will cease immediately.</p>
 <div className="py-3"></div>
-<p>⊳ 7. Limitation of Liability
+<p>⊳ 7. Limitation of Liability<br></br>
 In no event shall we be liable for any indirect, incidental, special, consequential, or punitive damages, including but not limited to loss of profits, data, or use, arising out of or in connection with your use of Gojo.</p>
 <div className="py-3"></div>
-<p>⊳ 8. Changes to Terms
+<p>⊳ 8. Changes to Terms<br></br>
 We reserve the right to update or modify these Terms of Service at any time. We will notify users of any material changes by posting the updated terms on our website or through other communication channels.</p>
 <div className="py-3"></div>
 <p>⊳ Contact Us: If you have any questions or concerns about this Terms of Service or our use of your personal information, please contact us at the discord support server provided within the bot's help menu or in footer.</p>
