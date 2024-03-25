f9cb3d6ace96b8587996bc905151b1291176a90b
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-03-25 13:03:25 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Index() {
@@ -97,7 +96,6 @@ Vote Gojo</a>
               </>
       
         </div>
-	<Analytics />
 	    <SpeedInsights />
       </>
   )
