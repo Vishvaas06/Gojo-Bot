45b66e180c84d5e8ddae3fa61c151bc346376c67
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-13 03:03:37 +0530

    Update gems.jsx

--- pages/gems.jsx
+++ pages/gems.jsx
@@ -15,24 +15,9 @@ export default function Partners() {
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
                 
-            <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/N7y3SE3prf">
-                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1037413396380921921/1226151004370894868/a_55a509d97567e36114d0f79f8c0abf4e.gif?ex=6623b91c&is=6611441c&hm=e540213f37a58471a4c47d2e111f907983b1ab2d7a3e446629f3a6a5f80c5b1c&=" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Vivi's Donation Community</h1>        
-                                <div className="fa-user flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                      700+ members
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                    </a>
-                    </div>
 
                         <div className="flex flex-col justify-center text-white rounded">
-            <a href="">
+                        <a href="">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://images-ext-1.discordapp.net/external/D-gbHRNijnx4nqO6pb8XTHZhUpuoS7h_dopszcDkeeg/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1094017858960363660/c99b7c8a0d7765faeb89eeca01e09b0f.webp?format=webp&width=473&height=473" />
                             <div>
