ed8bd9a31cd750a6fee78be7e0a9fe8f27598b8b
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-20 19:20:09 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect } from 'react'
 export default function Login() {
-  const state = generateRandomString(16);
+  function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+  const state = makeid(16)
   const uri = 'https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private%20playlist-read-collaborative&state=' + {state};
   window.location.replace(uri);
   return <>Redirecting...</>;
