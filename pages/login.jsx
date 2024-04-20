ce0c01516b888cbd9a8f07ddcdfa8e2fb7378f33
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-20 19:07:17 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,6 +2,7 @@ export default function Login() {
   useEffect(() => {
   const state = generateRandomString(16);
   window.location.replace('https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private%20playlist-read-collaborative&state=' + ${state});
+  }, []);
   return (
     <>Redirecting...</>
   );
