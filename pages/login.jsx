cce5dc9774a0f5ebd839edbde76109bdb449052c
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-20 19:33:36 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     return result;
   }
   let state = makeid(16)
-  const uri = `https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private%20playlist-read-collaborative&state=${state}`;
+  const uri = `https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private playlist-read-collaborative&state=${state}`;
   useEffect(() => {
   window.location.replace(uri);
   }, []);
