d20839c8086b3789ebf262c6b0cfafb9c735da00
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-21 00:27:04 +0530

    Update failed.jsx

--- pages/failed.jsx
+++ pages/failed.jsx
@@ -1,5 +1,5 @@
 const failed = () => {
-    return <>Failed while login to your account! ❌</>
+    return <>Login failed! Please check your credentials and try again. ❌</>
 }
 
 export default failed;
