bee6df014e52f49d86b6dcf37ad1f611c9f217c7
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-20 18:27:03 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,6 +1,6 @@
 var state = generateRandomString(16);
 
-export default function Tos() {
+export default function Login() {
   return (
     <Redirect to=`https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private%20playlist-read-collaborative&state=${state}`  />
   );
