c2dfdf2e8ffb258f3f2aa6773911dabc3d28ec7c
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-04-20 19:27:48 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -14,6 +14,8 @@ export default function Login() {
   }
   const state = makeid(16)
   const uri = 'https://accounts.spotify.com/authorize?response_type=code&client_id=a47de44f66a74e27ac5db06119628b2e&redirect_uri=http://gojobot.xyz/callback&scope=playlist-read-private%20playlist-read-collaborative&state=' + {state};
+  useEffect(() => {
   window.location.replace(uri);
+  }, []);
   return <>Redirecting...</>;
 }
