9444bf744e08db00fd4353cd21453994bbdfd574
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-05-03 04:04:11 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -45,46 +45,6 @@ Vote</a>
         </div>
       </div>
 
-
-
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
-          <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Gojo" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.com/oauth2/authorize?client_id=1167896630930985023" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite
-            </a>
-          </div>
-
-          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
-           
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-music text-2xl text-amber-500`} /> Quality</p>
-                  <p className="text-gray-500 line-clamp-4">Provides you High Quality music listening completely FREE with music filters.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-vr-cardboard text-2xl text-amber-500`} /> Controls</p>
-                  <p className="text-gray-500 line-clamp-4">Experience effortless control with Gojo's intuitive playback buttons. Skip, pause, adjust volume, and more, all at your fingertips.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-address-card text-2xl text-amber-500`} /> Profile & Badges</p>
-                  <p className="text-gray-500 line-clamp-4">Customizable music profiles showcasing your favourite song, songs played, time listened and achievement badges</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-rectangle-list text-2xl text-amber-500`} /> Charts</p>
-                  <p className="text-gray-500 line-clamp-4">Get access to global/regional weekly,daily & viral top charts.</p>
-                </div>
-              </div>
-          </div>
-      </div>
-
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
 		<img src="https://i.imgur.com/lDdcJDI.png" className="hidden lg:block rounded-xl shadow-xl shadow-black" />
@@ -132,6 +92,16 @@ Vote</a>
               </>
       
         </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+          <div className="col-span-4">
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Gojo" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
+            <a href="https://discord.com/oauth2/authorize?client_id=1167896630930985023" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Invite
+            </a>
+          </div>
+      </div>
 
 	<Analytics />
 	<SpeedInsights route={router.pathname} />;
